efffbb1ee10248fb1311e4d2e56934fbc3cc4e2d
Atharva <atharvaaher12outlook.com>
2025-08-10 16:13:58 +0530

    Add Storage.js Util

---
+++ utils/storage.js
@@ -0,0 +1,9 @@
+// utils/storage.js
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;  // Your service role key
+
+const supabase = createClient(supabaseUrl, supabaseServiceKey);
+
+module.exports = supabase.storage;

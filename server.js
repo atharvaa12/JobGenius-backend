c4d52214d5a14054fbdbfb729966decf4326c4a3
Atharva <atharvaaher12outlook.com>
2025-08-13 14:47:58 +0530

    allow all headers

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(
     origin: ['http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+   
   })
 );
 

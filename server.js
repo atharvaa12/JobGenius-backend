78f68fdadab3fb6c8bdda7eb8f31fe5f64226997
FlyBlaze009 <131971774+FlyBlaze009@users.noreply.github.com>
2025-09-01 17:21:10 +0530

    cors updated

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const HOST = '0.0.0.0';
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://jobportal-peach-ten.vercel.app/'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
    
